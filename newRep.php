dd53ec4ba399db128ac6c9370f88b29baa5491d3
nbala02 <63129564+nbala02@users.noreply.github.com>
2020-04-17 23:52:10 -0400

    Add files via upload

--- newRep.php
+++ newRep.php
@@ -1,57 +1,52 @@
 <?php
     session_start();
 
+    include('dbconnect.php');
+
+    $name = filter_input(INPUT_POST, 'name');
+    list($fname, $lname) = explode(" ", $name);
+    $address = filter_input(INPUT_POST, 'address');
+    $phone = filter_input(INPUT_POST, 'phone');
+    $salary = filter_input(INPUT_POST, 'salary');
+    $ytd = filter_input(INPUT_POST, 'ytd');
+    $comm = filter_input(INPUT_POST, 'comm');
+    $email = filter_input(INPUT_POST, 'email');
+    $password = filter_input(INPUT_POST, 'password');
+    $pin = "S";
+    
+    $i = 0; $digits = 5;
+    while($i < $digits)
+    {
+        //generate a random number between 0 and 9.
+        $pin .= mt_rand(1, 9);
+        $i++;
+    }
+
     if(isset($_SESSION['ldmanager1']))
     {
-        $dbconnect = mysqli_connect("127.0.0.1", "root", "csci318project", "dealer_one");
-
-        $code = filter_input(INPUT_POST, 'cust_no');
-        $fname = filter_input(INPUT_POST, 'fname');
-        $lname = filter_input(INPUT_POST, 'lname');
-        $email = filter_input(INPUT_POST, 'email');
-        $username = filter_input(INPUT_POST, 'username');
-        $password = filter_input(INPUT_POST, 'password');
-
-        $query = "SELECT * FROM customer WHERE cust_no = '$code'";
-        $output = $dbconnect->query($query);
-
-        if($output->num_rows != 0)
-        {
-            while($result = mysqli_fetch_assoc($output)) 
-            {
-                $id = $result['cust_no'];
-            }
-
-            if(isset($id) && $id == $code)
-            {
-                $sql = "INSERT INTO customeracc (fname, lname, email, username, password)
-                VALUES ('$fname', '$lname', '$email', '$username', '$password')";
-
-                $email = $_POST['email'];
-                $name = $_POST['fname'];
-
-                if ($dbconnect->query($sql) === TRUE) 
-                {
-                    $_SESSION['name'] = $email;
-                    $_SESSION['fname'] = $name;
-                    header('Location: custAccount.html');
-                } else 
-                {
-                    //echo "Something went wrong" . "<br>" . $dbconnect->error;
-                    //header('Location: registerPage.html');
-                    echo "<script>alert('Something went wrong'); window.location.href='registerPage.html';</script>";
-                }
-
-                $dbconnect->close();  
-            }
-        } else
-        {
-            //echo "Wrong Code" . "<br>" . $dbconnect->error;
-            //header('Location: register.html');
-            echo "<script>alert('Incorrect Customer ID'); window.location.href='registerPage.html';</script>";
-        }  
+        $connection = mysqli_connect("127.0.0.1", "root", "csci318project", "dealer_one");
+        $sql = "INSERT INTO representative (rep_no, name, address, phone, base_salary, ytd_sales, comm)
+                    VALUES ('$pin', '$name', '$address', '$phone', '$salary', '$ytd', '$comm')";
+        $location = "D1";
     } else if(isset($_SESSION['ldmanager2']))
     {
-        
+        $connection = mysqli_connect("127.0.0.1", "root", "csci318project", "dealer_two");
+        $sql = "INSERT INTO sales_person (sale_no, name, address, phone, comm, base_salary, ytdsales)
+                    VALUES ('$pin','$name', '$address', '$phone', '$comm', '$salary', '$ytd')";
+        $location = "D2";
     }
+
+    $query = "INSERT INTO employee (emp_no, fname, lname, email, password, location) 
+                VALUES ('$pin','$fname', '$lname', '$email', '$password', '$location')";
+
+    if ($connection->query($sql) === TRUE && $dbconnect->query($query) === TRUE) 
+    {
+        echo "<script>alert('New Person Added'); window.location.href='index.html';</script>";
+    }
+    else 
+    {
+        //echo "Wrong Code" . "<br>" . $dbconnect->error;
+        //echo "Wrong Code" . "<br>" . $connection->error;
+        echo "<script>alert('You already filled one'); window.location.href='addRep.html';</script>";
+    } 
 ?>
