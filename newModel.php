2a74938b547ae6a45d0e3624ae1d29f0c34a6998
aemunahmar <aemunahmar@hotmail.com>
2020-04-21 16:59:02 -0400

    Update newModel.php

--- newModel.php
+++ newModel.php
@@ -10,7 +10,7 @@
     $seat = filter_input(INPUT_POST, 'seat');
     $engine = filter_input(INPUT_POST, 'engine');
 
-    $connection = mysqli_connect("127.0.0.1", "root", "csci318project", "global_tables");
+    $connection = mysqli_connect("127.0.0.1", "root", "csci318project", "globalviews");
     $sql = "INSERT INTO model (model, price, type, gas_mileage, seat, engine)
             VALUES ('$model', '$price', '$type', '$mileage', '$seat', '$engine')";
 
