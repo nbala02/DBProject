793d7663c3f3ff62e71544c393264443c5c20ff1
aemunahmar <aemunahmar@hotmail.com>
2020-04-24 15:43:11 -0400

    Updated addTrans.html

--- dbconnect.php
+++ dbconnect.php
@@ -1,10 +1,27 @@
 <?php
-    // Create connection
-    $dbconnect = mysqli_connect("127.0.0.1", "root", "csci318project", "testingDB");
+    //Create connection
+    $dbconnect = mysqli_connect("127.0.0.1", "root", "", "testingDB");
+
+    //This section is for the addTrans.html page
+    if(isset($_SESSION['salesrep1']))
+    {
+        $connect = mysqli_connect("127.0.0.1", "root", "", "dealer_one");
+        $query1 = "SELECT rebate_no FROM rebate1";
+        $query2 = "SELECT serial_no FROM cars";
+    } else if(isset($_SESSION['salesrep2']))
+    {
+        $connect = mysqli_connect("127.0.0.1", "root", "", "dealer_two");
+        $query1 = "SELECT rebate_no FROM rebate2";
+        $query2 = "SELECT vehicle_no FROM autos";
+    }
+
+    $result1 = mysqli_query($connect, $query1);
+    $result2 = mysqli_query($connect, $query2);
     
-    // Check connection
-    if ($dbconnect->connect_error) 
+    //Check connection
+    if ($dbconnect->connect_error || $connect->connect_error)
     {
         die("Connection failed: " . $dbconnect->connect_error);
+        die("Connection failed: " . $connect->connect_error);
     } 
 ?>
