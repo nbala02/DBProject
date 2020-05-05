1dfc98b2f3c92ffbe4d6c063067a7f63445f50d1
nbala02 <63129564+nbala02@users.noreply.github.com>
2020-05-05 19:03:39 -0400

    Add files via upload

--- storeContactInfo.php
+++ storeContactInfo.php
@@ -1,16 +1,23 @@
 <?php
     session_start();
 
-    include('dbconnect.php');
+    $dbconnect = mysqli_connect("127.0.0.1", "root", "csci318project", "globalviews");
 
-    $email = filter_input(INPUT_POST, 'email');
-    $fname = filter_input(INPUT_POST, 'fname');
-    $lname = filter_input(INPUT_POST, 'lname');
+    $name = filter_input(INPUT_POST, 'name');
+    $address = filter_input(INPUT_POST, 'address');
     $phone = filter_input(INPUT_POST, 'phone');
-    $streetaddress = filter_input(INPUT_POST, 'streetaddress');
-    $city = filter_input(INPUT_POST, 'city');
-    $state = filter_input(INPUT_POST, 'state');
-    $zipcode = filter_input(INPUT_POST, 'zipcode');
+    $email = filter_input(INPUT_POST, 'email');
+
+    $pin = "C";
+    
+    //Assign random pin to new sales representative
+    $i = 0; $digits = 5;
+    while($i < $digits)
+    {
+        //generate a random number between 0 and 9.
+        $pin .= mt_rand(1, 9);
+        $i++;
+    }
 
     // Check connection
     if ($dbconnect->connect_error)
@@ -18,8 +25,8 @@
         die("Connection failed: " . $dbconnect->connect_error);
     }
 
-    $sql = "INSERT INTO contactInfo (email, fname, lname, phone, streetaddress, city, state, zipcode)
-    VALUES ('$email', '$fname', '$lname', '$phone', '$streetaddress', '$city', '$state', '$zipcode')";
+    $sql = "INSERT INTO potential_buyer (buyer_no, name, address, phone, email)
+    VALUES ('$pin', '$name', '$address', '$phone', '$email')";
 
     if ($dbconnect->query($sql) === TRUE)
     {
