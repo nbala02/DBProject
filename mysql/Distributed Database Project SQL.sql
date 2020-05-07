fe2e62378cc5ea92cc8b5e8eaee975eab5b7b973
aemunahmar <aemunahmar@hotmail.com>
2020-05-07 17:31:06 -0400

    Add files via upload

--- mysql/Distributed Database Project SQL.sql
+++ mysql/Distributed Database Project SQL.sql
@@ -3,29 +3,28 @@ use testingDB;
 
 create table employee
 (
-	emp_no			varchar(6), #Unique id
-    fname			varchar(50) NOT NULL, #first name
-    lname			varchar(50) NOT NULL, #last name
+	emp_no			varchar(6)   NOT NULL,  #Unique id
+    fname			varchar(50)  NOT NULL, #first name
+    lname			varchar(50)  NOT NULL, #last name
     email			varchar(100) NOT NULL, #email
-    password		varchar(20) NOT NULL, #password
-    location		varchar(2) NOT NULL, #which dealer
+    password		varchar(20)  NOT NULL, #password
+    location		varchar(2)   NOT NULL, #which dealer
     
     PRIMARY KEY	(emp_no)
 );
 
 create table customeracc
 (
-	customer_no		varchar(6), #Unique id
-	fname			varchar(50) NOT NULL, #first name
-    lname			varchar(50) NOT NULL, #last name
-    email			varchar(100) NOT NULL, #email
-    username		varchar(50) NOT NULL, #username used for login
-    password		varchar(20) NOT NULL, #password
-    location		varchar(2) NOT NULL, #which dealer
+	customer_no		varchar(6),   #Unique id
+	fname			varchar(50)   NOT NULL, #first name
+    lname			varchar(50)   NOT NULL, #last name
+    email			varchar(100)  NOT NULL, #email
+    username		varchar(50)   NOT NULL, #username used for login
+    password		varchar(20)   NOT NULL, #password
+    location		varchar(2)    NOT NULL, #which dealer
     
     PRIMARY KEY	(customer_no)
 );
 
 SELECT * FROM employee;
 SELECT * FROM customeracc;
-SELECT * FROM contactInfo;
--- mysql/dealer_two.sql
+++ mysql/dealer_two.sql
@@ -7,10 +7,10 @@ create table sales_person
     name                varchar(20) NOT NULL, #name of the salesperson
     address             varchar(50) NOT NULL, #address of salesperson
     phone				varchar(15) NOT NULL, #phone number of salesperson
-    comm				decimal(15) NOT NULL, # commission for the salesperson
-    base_salary			decimal(15) NOT NULL, # base salary for the salesperson
-    ytdsales			decimal(15) NOT NULL, # year to date amount for the salesperson
-
+    comm				decimal(15) NOT NULL, #commission for the salesperson
+    base_salary			decimal(15) NOT NULL, #base salary for the salesperson
+    ytdsales			decimal(15) NOT NULL, #year to date amount for the salesperson
+   
     PRIMARY KEY (sale_no)
 );
 
@@ -27,51 +27,49 @@ create table customer_d2
 create table rebate2
 (
 	rebate_no			varchar(10) NOT NULL, #unique id for the rebate no 
-    model               varchar(10) NOT NULL, #model for car
+    model               varchar(20) NOT NULL, #model for car
     rebate_amt			decimal(15) NOT NULL, #how much the rebate1 is
-    start_date 			DATE	   NOT NULL, # start date of rebate1
-    end_date 			DATE	   NOT NULL, # end date of the rebate1
+    start_date 			DATE	   	NOT NULL, #start date of rebate1
+    end_date 			DATE	   	NOT NULL, #end date of the rebate1
     expired				int			NOT NULL,
     
-    PRIMARY KEY(rebate_no),
-    FOREIGN KEY(model) REFERENCES globalviews.model(model)
+    PRIMARY KEY (rebate_no),
+    FOREIGN KEY (model) REFERENCES globalviews.model(model)
 );
 
 create table autos
 (
 	vehicle_no			varchar(10) NOT NULL, #Unique id for car
-    model               varchar(10) NOT NULL, #unique model for car
+    model               varchar(20) NOT NULL, #unique model for car
 	color				varchar(10) NOT NULL, #color of the car 
     autotrans			varchar(10)	NOT NUll, # yes or no if its autotransmission
     warehouse			varchar(20) NOT NULL, # warehouse city
-    rebate				varchar(3) NOT NULL, #Yes or not to if the rebate exists or not
+    rebate				varchar(3) 	NOT NULL, #Yes or not to if the rebate exists or not
 	
     PRIMARY KEY  (vehicle_no),
-    FOREIGN KEY(model) REFERENCES globalviews.model(model)
-
+    FOREIGN KEY (model) REFERENCES globalviews.model(model)
 );
 
 create table purchased_autos
 (
 	vehicle_no			varchar(10) NOT NULL, #Unique id for car
-    model               varchar(10) NOT NULL, #unique model for car
+    model               varchar(20) NOT NULL, #unique model for car
 	color				varchar(10) NOT NULL, #color of the car 
     autotrans			varchar(10)	NOT NUll, # yes or no if its autotransmission
     warehouse			varchar(20) NOT NULL, # warehouse city
     amount				decimal(15) NOT NULL, # warehouse city
 	
     PRIMARY KEY  (vehicle_no)
-
 );
 
 create table finance
 (
 	vehicle_no			varchar(10) NOT NULL, #Unique id for car
-    buyer_no         varchar(10) NOT NULL, #unique id for the customer 
+    buyer_no         	varchar(10) NOT NULL, #unique id for the customer 
     amount				decimal(15) NOT NULL, #how much the loan is
-    start_date 			DATE	   NOT NULL, # start date of loan
-    end_date 			DATE	   NOT NULL, # end date of the loan
-    months				int(20)	   NOT NULL, # how many months the loan is 
+    start_date 			DATE	   	NOT NULL, # start date of loan
+    end_date 			DATE	   	NOT NULL, # end date of the loan
+    months				int(20)	   	NOT NULL, # how many months the loan is 
     balance 			decimal(15) NOT NULL, #how much the balance is 
     
 	FOREIGN KEY(vehicle_no) REFERENCES purchased_autos(vehicle_no),
@@ -81,8 +79,9 @@ create table finance
 create table deal
 (
 	deal_no	            varchar(10) NOT NULL, #Unique id for transaction
-    rebate_no			 varchar(10) NULL, #unique id for the rebate no
-    sale_no              varchar(10) NOT NULL, #unique id for representative
+    rebate_no			varchar(10) NULL, #unique id for the rebate no 
+    package_no			varchar(2) 	NULL,
+    sale_no             varchar(10) NOT NULL, #unique id for representative 
     buyer_no		    varchar(10) NOT NULL, #unique id for the customer
     vehicle_no			varchar(10) NOT NULL, #Unique id for car
 	amount				decimal(15) NOT NULL, #how much the transaction is
